966857b187e68dc3d60bde9d9f939e4830585556
Diogo-Sabec <diogo.sabec@oktagon.com.br>
2023-06-29 22:50:37 -0300

    tela da roleta

--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,6 @@ from . import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
-<<<<<<< HEAD
     path('register/', views.register, name='register')
     
-=======
->>>>>>> 09c7e160ea59d154cc67e8f826af64a7847286b1
 ]
-
-
--- app/views.py
+++ app/views.py
@@ -7,13 +7,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-<<<<<<< HEAD
 def login(request):
     return render(request, 'login.html')
 
 def register(request):
     return render(request, 'register.html')
-=======
-def index(request):
-    return render(request, 'login.html')
->>>>>>> 09c7e160ea59d154cc67e8f826af64a7847286b1
