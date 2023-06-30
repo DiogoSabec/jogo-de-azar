3b8e47d7ce05e2277e55f459ecc3a389d693f95d
Diogo-Sabec <diogo.sabec@oktagon.com.br>
2023-06-29 22:44:21 -0300

    Tela da roleta

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register')
+    
 ]
