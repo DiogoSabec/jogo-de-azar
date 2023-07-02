2f6b2a9a309fee2a97f4815a8a8941835da84ada
JoaoBortolotti <joaomarcosbortolotti@gmail.com>
2023-07-02 08:02:33 -0300

    Register Login

--- app/views.py
+++ app/views.py
@@ -1,14 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from .models import *
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-
-def index(request):
-    return render(request, 'index.html')
+def register(request):
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username') 
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.get(username=username)
+        if user:
+            return HttpResponse('Já existe um usuário com este nome')
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        return redirect('http://127.0.0.1:8000/login/')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            login_django(request, user)
+            return redirect('http://127.0.0.1:8000/index/')
+        else:
+            return HttpResponse("errado")
 
-def register(request):
-    return render(request, 'register.html')
+@login_required 
+def index(request):
+    if request.user.is_authenticated:
+        return render(request, 'index.html')
+    return redirect('http://127.0.0.1:8000/login/')
+    
